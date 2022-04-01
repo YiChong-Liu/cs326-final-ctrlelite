79d5e3d67d14dcdba23f7dd53df6074a53bdd6c1
Conlan Cesar <ccesar@umass.edu>
2022-04-01 11:51:47 -0400

    Backend: aceptedMatch test endpoint

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+// Matches
+app.post('/matches/acceptedMatch', (req, res) => {
+  // TODO authenticate & authorize via JWT
+
+  // Get the user and their proposed partner
+  const otherGuy = req.query.secondaryUserID;
+  const myself = req.query.primaryUserID;
+
+  // TODO database call to run them
+
+  // TODO return HTTP header / JSON response with real data
+  res.status(200).send({otherUser: otherGuy, matchAccepted: true});
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
