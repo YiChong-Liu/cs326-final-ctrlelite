118e6a6402bbda728cc7c71295e888a148db13d9
Conlan Cesar <ccesar@umass.edu>
2022-04-15 11:59:32 -0400

    Use $PORT env var when available

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import backendRouter from './backend.js';
 
 // Initialize express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use('/', express.static('./src'));
 app.use('/api', backendRouter);
