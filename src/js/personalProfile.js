7af7c90dffbf9dfb36564e0fe38f4498228f5603
Liam Neal Reilly <lhnealreilly@gmail.com>
2022-04-15 12:21:01 -0400

    Added jwt to saveProfile

--- src/js/personalProfile.js
+++ src/js/personalProfile.js
@@ -1,4 +1,4 @@
-
+import { parseJwt } from "./main.js";
 
 const profilePicture = document.getElementById('profile-picture');
 const uploadImg = document.getElementById('img-upload');
@@ -18,8 +18,8 @@ uploadImg.addEventListener("change", (e) => {
 });
 
 saveProfile.addEventListener("click", async(e) => {
-
-    let response = await fetch('/api/update/userProfile', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({'userID': "user", 'profile': {userName: nameField.value, bio: bio.value, profilePicture: profilePicture.src}})});
+    let userID = parseJwt(document.cookie).user;
+    let response = await fetch('/api/update/userProfile', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({'userID': userID, 'profile': {userName: nameField.value, bio: bio.value, profilePicture: profilePicture.src}})});
     if(response.ok){
         let responseJSON = await response.json();
         if(responseJSON.worked){
