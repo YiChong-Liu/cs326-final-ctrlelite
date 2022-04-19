c99bb3546f4ed1d9ae638b849cb4b2b85b83dbe9
benjamin tufano <btufano@umass.edu>
2022-04-19 18:17:05 -0400

    fixed signup-crud

--- src/js/signup.js
+++ src/js/signup.js
@@ -6,3 +6,20 @@ $('#registerCheck').click(function() {
     	$('#submit_button').attr('disabled', 'disabled');
     }
 });
+
+
+function signupUser(e, p) {
+	let response = await fetch('/users/newUser', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({'email': e, 'password': p})});
+    if(response.ok){
+        let responseJSON = await response.json();
+        console.log(responseJSON);
+    } else {
+        console.log('ERROR: failed to update preferences');
+    }
+}
+
+document.getElementById('submit_button').addEventListener('click', function(x) {
+	let e = document.getElementById('registerEmail').value;
+	let p = document.getElementById('registerPassword').value;
+	signupUser(e, p);
+});
