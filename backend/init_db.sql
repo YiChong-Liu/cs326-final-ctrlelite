b4ec3454c9e54eb902df7b7ebd57fd54fd862564
Liam Neal Reilly <lhnealreilly@gmail.com>
2022-04-26 16:33:31 -0400

    Added initial database sql

---
+++ backend/init_db.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS users(
+    uID varchar(40) NOT NULL,
+    password varchar(450) NOT NULL,
+    email varchar(50) NOT NULL,
+    PRIMARY KEY(uID)
+);
+
+CREATE TABLE IF NOT EXISTS matches(
+    uID1 varchar(40) NOT NULL,
+    uID2 varchar(40) NOT NULL,
+    u1Accept BIT DEFAULT 0,
+    u2Accept BIT DEFAULT 0,
+    PRIMARY KEY(uID1, uID2)
+);
+
+CREATE TABLE IF NOT EXISTS userPreferences(
+    uID varchar(40) NOT NULL,
+    state varchar(40),
+    city varchar(40),
+    gender varchar(20), 
+    genderImportance decimal,
+    age integer,
+    ageImportance decimal,
+    housingType varchar(20),
+    housingTypeImportance decimal, 
+    noiseLevel varchar(30), 
+    noiseImportance decimal,
+    cleanliness varchar(40),
+    cleanlinessImportance decimal,
+    sharing varchar(40),
+    sharingImportance decimal,
+    PRIMARY KEY(uID)
+);
+
+CREATE TABLE IF NOT EXISTS userProfiles(
+    uID varchar(40) NOT NULL,
+    profileJSON varchar(2048),
+    PRIMARY KEY(uID)
+);
+
+CREATE TABLE IF NOT EXISTS chat(
+    uID1 varchar(40) NOT NULL,
+    uID2 varchar(40) NOT NULL,
+    msg varchar(200),
+    PRIMARY KEY(uID1, uID2)
+);
+
