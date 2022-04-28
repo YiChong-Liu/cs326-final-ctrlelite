a33eaec3ad91d03000ba4ce24419a7f678133990
lhnealreilly <lhnealreilly@umass.edu>
2022-04-27 22:56:35 -0400

    Added accept match event listener and initial implementation of accept match function

--- src/js/matches.js
+++ src/js/matches.js
@@ -1,6 +1,8 @@
 const matchDIV = document.getElementById('matchList');
 
 function newMatch(user_data){
+    let profileData = JSON.parse(user_data.profilejson);
+
     let wrapper  = document.createElement('li');
     wrapper.classList.add("list-group-item");
     let contentDiv = document.createElement('div');
@@ -12,7 +14,7 @@ function newMatch(user_data){
     contentDiv.appendChild(profilePictureDIV);
     let profilePicture = document.createElement('img');
     profilePicture.classList.add('rounded-circle', 'w-100');
-    profilePicture.src = user_data.profile.profilePicture;
+    profilePicture.src = profileData.profilePicture;
     profilePictureDIV.appendChild(profilePicture);
 
     let profileLinkDiv = document.createElement('div');
@@ -20,8 +22,8 @@ function newMatch(user_data){
     contentDiv.appendChild(profileLinkDiv);
     let profileLink = document.createElement('a');
     profileLink.classList.add('link-primary');
-    profileLink.innerHTML = `<h2>${user_data.profile.userName}</h2>`;
-    profileLink.href = "profile.html?userID=" + user_data.user_ID;
+    profileLink.innerHTML = `<h2>${profileData.userName}</h2>`;
+    profileLink.href = "profile.html?userID=" + user_data.user;
     profileLinkDiv.appendChild(profileLink);
 
     let spacingDIV = document.createElement('div');
@@ -35,7 +37,7 @@ function newMatch(user_data){
     let chatButton = document.createElement('a');
     chatButton.classList.add("btn", "btn-primary", "btn-block", "mb-3");
     chatButton.innerHTML = "Chat";
-    chatButton.href = `chat.html?userID2=${user_data.profile.userName}`;
+    chatButton.href = `chat.html?userID2=${profileData.userName}`;
     buttonDIV.appendChild(chatButton);
     let removeButton = document.createElement('a');
     removeButton.classList.add("btn", "btn-primary", "btn-danger", "mb-3")
