146f03931a4b75d3c580d02180d6fd3e732b8a1d
Yichong Liu <yichongliu23@gmail.com>
2022-04-29 11:14:22 -0400

    add the confirm password form

--- src/js/signup.js
+++ src/js/signup.js
@@ -8,19 +8,36 @@ $('#registerCheck').click(function() {
 });
 
 
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    alert("Passwords Don't Match! Try again.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+
+document.getElementById('submit_button').addEventListener('click', function(x) {
+    if(validatePassword()) {
+        let e = document.getElementById('email').value;
+        let p = document.getElementById('password').value;
+        let n = document.getElementById('username').value;
+        signupUser(e, p, n);
+    }
+});
 
 async function signupUser(e, p, n) {
 	let response = await fetch('/api/users/newUser', {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({'email': e, 'password': p, 'username': n})});
     if(response.ok){
-        window.location = response.url; 
+        window.location = response.url;
     } else {
         alert('User with that email exists');
     }
 }
-
-document.getElementById('submit_button').addEventListener('click', function(x) {
-	let e = document.getElementById('email').value;
-	let p = document.getElementById('password').value;
-    let n = document.getElementById('username').value;
-	signupUser(e, p, n);
-});
